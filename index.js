1978fa6aa1a86f0b2474487c5f8f69dfb2946684
Arafat-erasung <arafaterasung@gmail.com>
2022-05-08 23:04:44 +0200

    added media query for small devices @375px max-width

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function toggleFeatures() {
 
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(e) {
-  if (!e.target.matches('.dropbtn')) {
+  if (!e.target.matches('dropbtn')) {
   let myDropdown = document.getElementById("dropdown");
     if (myDropdown.classList.contains('show')) {
       myDropdown.classList.remove('show');
