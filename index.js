ca769578d6612aa5995642da1c5d1af87d0e8dd1
Arafat-erasung <arafaterasung@gmail.com>
2022-05-12 01:07:34 +0200

    added open function to the dropdowns

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const openMenu = document.querySelector('.menu')
 const closeMenu = document.querySelector('.close-menu')
 const featuresButton = document.querySelector('.features-drop')
-const featuresContent = document.getElementById('features')
+const featuresContent = document.querySelector('.features-content')
 const companyButton = document.querySelector('.company-drop')
-const companyContent = document.getElementById('company')
+const companyContent = document.querySelector('.company-content')
 const sidenavigation = document.querySelector('.sidenav')
 const featuresArrowDown = document.getElementById('arrow-down')
 const companyArrowDown = document.getElementById('com-arrow-down')
@@ -14,22 +14,24 @@ closeMenu.addEventListener('click', close)
 featuresButton.addEventListener('click', featuresDropdown)
 companyButton.addEventListener('click', companyDropdown)
 
-function featuresDropdown(){
-    // console.log("dropped")
-    featuresArrowDown.src = "images/icon-arrow-up.svg";
 
+function featuresDropdown(){
+    featuresArrowDown.src = "images/icon-arrow-up.svg";
+    featuresContent.style.display = 'block'
 }
 
+
+
 function companyDropdown() {
-    // console.log("comp-drop")
-    companyArrowDown.src = "images/icon-arrow-up.svg";
+    companyArrowDown.src = "images/icon-arrow-up.svg"
+    companyContent.style.display = 'block'
 }
 
 
 function show() {
-    sidenavigation.style.display = 'block';
+    sidenavigation.style.display = 'block'
 }
 
 function close() {
-    sidenavigation.style.display = 'none';
+    sidenavigation.style.display = 'none'
 }
